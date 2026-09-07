ad817a02af6016a2916a0370bf781279fc18abbf
.gitValentina <you@example.com>
2026-09-07 17:50:23 -0500

    Se agregan metodos a Automata

--- src/main/java/com/automindx/modelo/Automata.java
+++ src/main/java/com/automindx/modelo/Automata.java
@@ -19,4 +19,63 @@ public class Automata {
         this.estadosFinales = new ArrayList<>();
     }
 
+    public void agregarEstado(Estado estado) {
+        this.estados.add(estado);
+    }
+
+    public void agregarSimbolo(char simbolo) {
+        this.alfabeto.add(simbolo);
+    }
+
+    public void agregarTransicion(Transicion transicion) {
+        this.transiciones.add(transicion);
+    }
+
+    public void establecerEstadoInicial(Estado estado) {
+        this.estadoInicial = estado;
+    }
+
+    public void agregarEstadoFinal(Estado estado) {
+        this.estadosFinales.add(estado);
+    }
+
+    public List<Estado> getEstados() {
+        return estados;
+    }
+
+    public void setEstados(List<Estado> estados) {
+        this.estados = estados;
+    }
+
+    public Set<Character> getAlfabeto() {
+        return alfabeto;
+    }
+
+    public void setAlfabeto(Set<Character> alfabeto) {
+        this.alfabeto = alfabeto;
+    }
+
+    public List<Transicion> getTransiciones() {
+        return transiciones;
+    }
+
+    public void setTransiciones(List<Transicion> transiciones) {
+        this.transiciones = transiciones;
+    }
+
+    public Estado getEstadoInicial() {
+        return estadoInicial;
+    }
+
+    public void setEstadoInicial(Estado estadoInicial) {
+        this.estadoInicial = estadoInicial;
+    }
+
+    public List<Estado> getEstadosFinales() {
+        return estadosFinales;
+    }
+
+    public void setEstadosFinales(List<Estado> estadosFinales) {
+        this.estadosFinales = estadosFinales;
+    }
 }
