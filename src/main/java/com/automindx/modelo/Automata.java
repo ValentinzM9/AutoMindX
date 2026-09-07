54e237fb08738b81d8bbbe7b3f4b46e7e66f4d6e
.gitValentina <you@example.com>
2026-09-06 22:01:37 -0500

    creacion de la clase automata y sus atributos

---
+++ src/main/java/com/automindx/modelo/Automata.java
@@ -0,0 +1,15 @@
+package main.java.com.automindx.modelo;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Automata {
+    private List<Estado> estados;
+    private Set<Character> alfabeto;
+    private List<Transicion> transiciones;
+    private Estado estadoInicial;
+    private List<Estado> estadosFinales;
+
+}
