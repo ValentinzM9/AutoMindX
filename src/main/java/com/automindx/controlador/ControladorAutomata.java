858a07d5994d209ab20bf7f20707081946539404
.gitValentina <you@example.com>
2026-09-07 21:01:04 -0500

    Se agregan atrivitos y metodos al controlador

--- src/main/java/com/automindx/controlador/ControladorAutomata.java
+++ src/main/java/com/automindx/controlador/ControladorAutomata.java
@@ -1,5 +1,49 @@
 package com.automindx.controlador;
 
-public class ControladorAutomata {
+import com.automindx.modelo.Automata;
+import com.automindx.modelo.Estado;
+import com.automindx.modelo.Transicion;
+import com.automindx.modelo.Validador;
 
+public class ControladorAutomata {
+    
+    private Automata automata;
+    private Validador validador;
+
+    public ControladorAutomata() {
+        this.automata = new Automata();
+        this.validador = new Validador(automata);
+    }
+
+    public void agregarEstado(Estado estado) {
+        automata.agregarEstado(estado);
+    }
+
+    public void agregarSimbolo(char simbolo) {
+        automata.agregarSimbolo(simbolo);
+    }
+
+    public void agregarTransicion(Transicion transicion) {
+        automata.agregarTransicion(transicion);
+    }
+
+    public void establecerEstadoInicial(Estado estado) {
+        automata.establecerEstadoInicial(estado);
+    }
+
+    public void agregarEstadoFinal(Estado estado) {
+        automata.agregarEstadoFinal(estado);
+    }
+
+    public boolean validarCadena(String cadena) {
+        return validador.validar(cadena);
+    }
+
+    public Automata getAutomata() {
+        return automata;
+    }
+
+    public Validador getValidador() {
+        return validador;
+    }
 }
