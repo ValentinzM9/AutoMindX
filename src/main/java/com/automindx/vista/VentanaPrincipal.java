9866925efecc474a281511224a6a1f07a37d20bf
.gitValentina <you@example.com>
2026-09-10 11:06:23 -0500

    Se crea la primera clase de la vista y de ajustan concexiones

---
+++ src/main/java/com/automindx/vista/VentanaPrincipal.java
@@ -0,0 +1,16 @@
+package com.automindx.vista;
+
+import javax.swing.JFrame;
+
+public class VentanaPrincipal extends JFrame {
+
+    public VentanaPrincipal() {
+
+        setTitle("AutoMindX - Simulador de Autómatas");
+        setSize(1000, 700);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+    }
+}
